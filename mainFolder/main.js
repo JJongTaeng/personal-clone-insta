dd28d40f9ef90fec1850f40e23c0efec8399c2de
인태 <joint14@naver.com>
2020-09-11 02:39:38 +0900

    commit

--- mainFolder/main.js
+++ mainFolder/main.js
@@ -2,14 +2,16 @@ const checkLog = console.log;
 const express = require('express');
 const path = require('path');
 const mysql = require('mysql');
+var multer  = require('multer')
 const fs = require('fs').promises;
+const fs2 = require('fs');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 const app = new express();
+let postImageLink;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 app.use(session({
   secret: 'akdfhasdjflj!@#!@@@', // 노출되어서 안되는 꼭 넣어야하는 옵션
   resave: false, // false로 두기
@@ -25,9 +27,18 @@ const db = mysql.createConnection({
 });
 // DB연결
 db.connect();
-
-
-
+app.use((req, res, next)=>{
+  db.query(`select * from post order by post_id desc limit 1;`, async (err, data)=>{
+    if(data.length ===0){
+      req.postImageLink = 1;
+      postImageLink= req.postImageLink;
+    } else {
+      req.postImageLink = data[0].post_id + 1;
+      postImageLink= req.postImageLink;
+    }
+    next();
+  });
+})
 app.get('/', async (req, res)=>{
   const data = await fs.readFile('./public/html/login.html');
   req.session.is_logined = false;
@@ -56,19 +67,19 @@ app.get('/main_data', async (req, res)=>{
     name: req.session.realname,
     nick: req.session.nickname,
     images: {},
+    post: [],
   };
   let index;
-  db.query(`select * from post where id = '${user_data.id}'`,async (err, data)=>{
+  db.query(`select post.post_id, id, nickname, content, upload_date from post left join post_content on post.post_id = post_content.post_id where id in (select following_id from following where id ="${user_data.id}");`,async (err, data)=>{
     if(data.length===0){
+      user_data.profile = await fs.readdir(`./public/${user_data.id}`);
       return res.end(JSON.stringify(user_data));
     }
     for(let i=0; i<data.length; i++) {
       // console.log(i);
       const imageLink = await fs.readdir(`./public/${data[i].post_id}`);
       user_data.profile = await fs.readdir(`./public/${user_data.id}`);
-      console.log(user_data.profile);
       index = data[i].post_id;
-      console.log(index);
       user_data.images[index] = Array.from(imageLink);
       if(i === data.length-1){
         user_data.post = data;
@@ -77,6 +88,7 @@ app.get('/main_data', async (req, res)=>{
     }
   })
 })
+
 app.get('/logout', async (req, res)=>{
   await req.session.destroy((err)=>{
     return res.end('logout');
@@ -108,11 +120,72 @@ app.post('/login', async (req, res)=>{
 app.post('/signup_process', (req, res)=> {
   const user = req.body;
   db.query(`insert into user (id, password, nickname, name) values ('${user.id}', '${user.password}', '${user.nickname}', '${user.name}');`,(err, data)=>{
-    if(err){
-      res.send('회원가입 실패 - 다시해주십시오');
-    }
-    res.redirect('/');
+    db.query(`insert into following values ('${user.id}','${user.id}')`, (err, data2)=>{
+      if(err){
+        res.send('회원가입 실패 - 다시해주십시오');
+      }
+      fs2.mkdirSync(`./public/${user.id}`);
+      res.redirect('/');
+    })
   })
+})
+let fileIndex =1;
+let upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, `./public/${postImageLink}`);
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, fileIndex++ + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
 
+app.post('/insert', async (req, res, next)=>{
+  try {
+    fs2.readdirSync(`./public/${req.postImageLink}`);
+    
+  } catch (error) {
+    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+    fs2.mkdirSync(`./public/${req.postImageLink}`);
+  }
+  console.log('폴더 생성후 파일 저장하러갑니다!')
+  next();
+},upload.array('images'), (req, res) => {    
+  // console.log(req.files, req.body); 
+  fileIndex = 1;
+  const body = req.body;
+  db.query(`insert into post (post_id, id, nickname) values ('${req.postImageLink}', '${req.session.idname}', '${req.session.nickname}')`, (err, data1)=>{
+    db.query(`select * from post order by post_id desc limit 1;`, async(err, data2)=>{
+      db.query(`insert into post_content (post_id, content) values ('${data2[0].post_id}', '${body.content}')`, (err, data3)=>{
+        res.redirect('/main');   
+      })
+    })
+  });
+});
+
+app.post('/delete', (req, res)=>{
+  const user = req.body;
+  req.session.deletePostId = user;
+  return res.end();
+});
+app.post('/delete_process', (req, res)=>{
+  console.log(req.session.deletePostId.userPostId[1]);
+  db.query(`delete from post where post_id = ${req.session.deletePostId.userPostId[1]}`,async (err, data1)=>{
+    db.query(`delete from post_content where post_id = ${req.session.deletePostId.userPostId[1]}`,async (err, data2)=>{
+      try{
+        const filename = await fs.readdir(`./public/${req.session.deletePostId.userPostId[1]}`);
+        for(let i=0; i<filename.length; i++){
+          await fs.unlink(`./public/${req.session.deletePostId.userPostId[1]}/${filename[i]}`);
+        }
+        await fs.rmdir(`./public/${req.session.deletePostId.userPostId[1]}`)
+        return res.end();
+      }catch {
+  
+      }
+    });
+  })
 })
 app.listen(3000,()=>console.log('3000 포트 대기'))
